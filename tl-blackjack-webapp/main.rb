5819b7859a4ce89bc1d7dee181234b63afcb4ac7
ckib16 <ckib16@gmail.com>
2014-09-21 11:45:06 -0400

    added regular and nested template views

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -12,3 +12,15 @@ end
 get '/home' do
   "I'm the home page biatch!!"
 end
+
+get '/lesson' do
+  "I've rendered text like the lesson asked me to"
+end
+
+get '/template' do
+  erb :my_template
+end
+
+get '/nested_template' do
+  erb :"users/profile"
+end

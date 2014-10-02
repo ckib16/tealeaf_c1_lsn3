16102e4506854940a0feca72dd7469785db2085f
ckib16 <ckib16@gmail.com>
2014-10-01 22:20:52 -0400

    Fully ajaxified

--- sinatra_blackjack/main.rb
+++ sinatra_blackjack/main.rb
@@ -55,20 +55,20 @@ helpers do
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] = session[:player_pot] + session[:player_bet]
-    @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
+    @winner = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
   end
 
   def loser!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
     session[:player_pot] = session[:player_pot] - session[:player_bet]
-    @error = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
+    @loser = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
   end 
 
   def tie!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
-    @success = "<strong> #{session[:player_name]} and Dealer tie.</strong> #{msg}"
+    @winner = "<strong> #{session[:player_name]} and Dealer tie.</strong> #{msg}"
   end
 
 end
@@ -141,10 +141,10 @@ post '/game/player/hit' do
   if player_total == BLACKJACK_AMOUNT
     winner!("#{session[:player_name]} hit Blackjack!")
   elsif player_total > BLACKJACK_AMOUNT
-    loser!("Looks like #{session[:player_name]} busted at #{player_total}.")
+    loser!("It looks like #{session[:player_name]} busted at #{player_total}.")
   end
 
-  erb :game
+  erb :game, layout: false
 end
 
 post '/game/player/stay' do
@@ -170,7 +170,7 @@ get '/game/dealer' do
     @show_dealer_hit_button = true
   end
       
-  erb :game  
+  erb :game, layout: false
 end
 
 post '/game/dealer/hit' do
@@ -192,7 +192,7 @@ get '/game/compare' do
     tie!("Both #{session[:player_name]} and the dealer stayed at #{player_total}")
   end
 
-  erb :game
+  erb :game, layout: false
 end
 
 get '/game_over' do
